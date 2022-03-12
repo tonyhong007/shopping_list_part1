a548db579554a8a605fdb451e833c651e26998f3
Tony <tshong@berkeley.edu>
2022-03-12 03:47:07 -0800

    skeleton code

---
+++ Shopping List/Shopping List/CustomCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCell.swift
+//  Shopping List
+//
+//  Created by Tony Hong on 3/12/22.
+//
+
+import SwiftUI
+
+struct CustomCell: View {
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+

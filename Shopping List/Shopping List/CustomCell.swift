59cf2d5e3f1d59a86e9ab6aea63458a15193265d
Tony <tshong@berkeley.edu>
2022-03-12 03:48:45 -0800

    Skeleton code

--- Shopping List/Shopping List/CustomCell.swift
+++ Shopping List/Shopping List/CustomCell.swift
@@ -9,9 +9,8 @@ import SwiftUI
 
 struct CustomCell: View {
     
-    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello World")
     }
 }
 
